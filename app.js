89318d4290d010cc9629ab8823db7ab6f6606808
TheWussPuss <kapsmudit@gmail.com>
2019-03-26 07:37:44 +0530

    TheWussPuss

---
+++ app.js
@@ -0,0 +1,215 @@
+const express = require('express')
+const cors = require('cors')
+let models = require('./models')
+let app = express()
+app.use(express.urlencoded({
+	extended: true
+}))
+app.use(express.json())
+app.use(cors())
+app.locals.categories = models.category_samples
+app.locals.products = models.product_samples
+
+app.get('/', (req, res) => {
+	res.json('Home')
+})
+
+app.get('/products', (req, res) => {
+	res.send(
+		app.locals.products
+			.filter(value => RegExp(req.query.name || '', 'i').test(value.name))
+			.map(value => value.stage())
+	)
+})
+
+app.post('/products', (req, res) => {
+	console.log(req.body);
+	
+	if (!req.body) {
+		res.status(400).send({
+			success: false,
+			message: "Product not posted"
+		})
+		return
+	}
+	let category = app.locals.categories.find(cat => cat.id == req.body.category_id)
+	let product = req.body
+	if (category && product.name && product.brand && product.price > 0) {
+		let prod = new models.Product(
+			product.name,
+			category,
+			product.brand,
+			product.price,
+			product.attributes || {}
+		)
+		app.locals.products.push(prod)
+		res.send({
+			success: true,
+			id: prod.id,
+			uri: `/products/${prod.id}`
+		})
+	} else {
+		res.status(400).send({
+			success: false,
+			message: "Category with given id doesn't exist"
+		})
+	}
+})
+
+app.get('/products/:id', (req, res) => {
+	let prod = app.locals.products.find(value => value.id == req.params.id)
+	if (prod) {
+		res.send(prod.stage())
+	} else {
+		res.status(404).send()
+	}
+})
+
+app.delete('/products/:id', (req, res) => {
+	if (req.params.id == 0) {
+		res.status(403).send('Cannot delete root category')
+		return;
+	}
+	let prod_index = app.locals.products.findIndex(prod => prod.id == req.params.id)
+	if (prod_index == -1) {
+		res.status(404).send({
+			success: false,
+			message: 'Product not found'
+		})
+	} else {
+		let prod = app.locals.products.splice(prod_index, 1)
+		console.log(prod);
+		let cat = app.locals.categories.find(ct => ct.id == prod[0].category_id)
+		cat.removeProduct(prod.id)
+		res.send({
+			success: true,
+			message: 'deleted'
+		})
+	}
+})
+
+app.put('/products/:id', (req, res) => {
+	let prod = app.locals.products.find(value => value.id == req.params.id)
+	if (prod) {
+		let product  = req.body
+		prod.name = product.name || prod.name
+		let cat = app.locals.categories.find(cat => cat.id = product.category_id)
+		if (cat) {
+			app.locals.categories.find(cat => cat.id = prod.category_id).removeProduct(prod.id)
+			cat.product_ids.push(prod.id)
+			prod.category_id = cat.id
+		}
+		prod.brand = product.brand || prod.brand
+		prod.price = product.price || prod.price
+		Object.keys(product.attributes).forEach(key => {
+			prod.attributes[key] = product.attributes[key] || prod.attributes[key]
+		})
+		res.send(prod.stage())
+	} else {
+		res.status(404).send()
+	}
+})
+
+app.post('/products/:id', (req, res) => {
+	let prod = app.locals.products.find(value => value.id == req.params.id)
+	if (prod) {
+		let product_instances = []
+		for (let i = 0; i < req.body.instances; i++) {
+			product_instances.push(prod.newProductInstance())
+		}
+		res.send(product_instances)
+	} else {
+		res.status(404).send()
+	}
+})
+
+
+
+
+app.get('/categories', (req, res) => {
+	res.send(
+		app.locals.categories
+		.filter(value => RegExp(req.query.name || '', 'i').test(value.name))
+		.map(value => value.stage())
+	)
+})
+
+app.post('/categories', (req, res) => {
+	if (!req.body) {
+		res.status(400).send({
+			success: false,
+			message: "Category not posted"
+		})
+		return
+	}
+	let parent = app.locals.categories.find(cat => cat.id == req.body.parent_id)
+	let category = req.body
+	category.tax = category.tax || models.DEFAULT_TAX
+	if (parent && category.name && category.tax >= 0 && category.tax <= 100) {
+		let cat = new models.Category(
+			category.name,
+			parent,
+			category.brand,
+		)
+		app.locals.categories.push(cat)
+		res.send({
+			success: true,
+			id: cat.id,
+			uri: `/categories/${cat.id}`
+		})
+	} else {
+		res.status(400).send({
+			success: false,
+			message: "Parent category with given id doesn't exist or tax is invalid"
+		})
+	}
+})
+
+app.get('/categories/:id', (req, res) => {
+	let cat = app.locals.categories.find(value => value.id == req.params.id)
+	if (cat) {
+		res.send(cat.stage())
+	} else {
+		res.status(404).send()
+	}
+})
+
+app.delete('/categories/:id', (req, res) => {
+	let cat_index = app.locals.categories.findIndex(cat => cat.id == req.params.id)
+	if (cat_index == -1) {
+		res.status(404).send({
+			success: false,
+			message: 'Category not found'
+		})
+	} else {
+		let cat = app.locals.categories.splice(cat_index, 1)
+		app.locals.products = app.locals.products.filter(prod => !(prod.id in cat[0].product_ids))
+		app.locals.categories.filter(ct => ct.parent_id == cat.id).forEach(ct => ct.parent_id = 0)
+		res.send({
+			success: true,
+			message: 'deleted'
+		})
+	}
+})
+
+app.put('/categories/:id', (req, res) => {
+	let cat = app.locals.categories.find(value => value.id == req.params.id)
+	if (cat) {
+		let category = req.body
+		cat.name = category.name || cat.name
+		let parent = app.locals.categories.find(ct => ct.parent_id == category.parent_id)
+		if (parent) {
+			app.locals.categories.find(ct => ct.parent_id == cat.parent_id).removeChild(cat.id)
+			parent.children_ids.push(cat.id)
+			cat.parent_id = parent.id
+		}
+		cat.tax = category.tax || cat.tax
+		res.send(cat.stage())
+	} else {
+		res.status(404).send()
+	}
+})
+
+app.listen(3000, () => {
+	console.log('Started on port 3000');
+})
--- models.js
+++ models.js
@@ -5,14 +5,54 @@ class Category {
 		this.children_ids = []
 		this.product_ids = []
 		this.tax = tax || (parent ? parent.tax : DEFAULT_TAX) || DEFAULT_TAX
-		Category.count = Category.count ? Category.count + 1 : 1
+		Category.count = Category.count || 0
 		this.id = Category.count
+		Category.count++
 		if (parent) {
 			this.parent_id = parent.id
-			parent.children.push(this.id)
+			parent.children_ids.push(this.id)
 		} else {
 			this.parent_id = 0
 		}
+	}
+	stage() {
+		let staged_category = {
+			id: this.id,
+			name: this.name,
+			parent_id: this.parent_id,
+			tax: this.tax,
+			children_ids: this.children_ids,
+			product_ids: this.product_ids,
+			links: [
+				{
+					rel: 'self',
+					href: `/categories/${this.id}`,
+					method: `GET`
+				},
+				{
+					rel: 'parent',
+					href: `/categories/${this.parent_id}`,
+					method: `GET`
+				},
+				{
+					rel: 'update',
+					href: `/categories/${this.id}`,
+					method: `PUT`
+				},
+				{
+					rel: 'delete',
+					href: `/categories/${this.id}`,
+					method: `DELETE`
+				},
+			]
+		}
+		return staged_category
+	}
+	removeProduct(id) {
+		this.product_ids = this.product_ids.filter(pid => pid != id)
+	}
+	removeChild(id) {
+		this.children_ids = this.children_ids.filter(cid => cid != id)
 	}
 }
 
@@ -27,11 +67,49 @@ class Product {
 		this.instance_count = 0
 		if (category) {
 			this.category_id = category.id
-			category.products.push(this.id)
+			category.product_ids.push(this.id)
 		}
 	}
 	newProductInstance() {
 		return this.id.toString().padStart(5, '0') + "-" + (++this.instance_count).toString().padStart(5, '0')
+	}
+	stage() {
+		let staged_product = {}
+		staged_product.id = this.id
+		staged_product.name = this.name
+		staged_product.brand = this.brand
+		staged_product.attributes = this.attributes
+		staged_product.price = this.price
+		staged_product.category_id = this.category_id
+		staged_product.links = [
+			{
+				rel: 'self',
+				href: `/products/${this.id}`,
+				method: 'GET'
+			},
+			{
+				rel: 'category',
+				href: `/categories/${this.category_id}`,
+				method: 'GET'
+			},
+			{
+				rel: 'instantiate',
+				href: `/products/${this.id}`,
+				method: 'POST'
+			},
+			{
+				rel: 'delete',
+				href: `/products/${this.id}`,
+				method: 'DELETE'
+			},
+			{
+				rel: 'update',
+				href: `/products/${this.id}`,
+				method: 'PUT'
+			},
+			
+		]
+		return staged_product
 	}
 }
 /* 
@@ -44,3 +122,19 @@ attributes = {
 	...
 }
  */
+exports.Category = Category
+exports.Product = Product
+exports.DEFAULT_TAX = DEFAULT_TAX
+
+let root_cat = new Category('ROOT')
+let category_samples = [
+	root_cat,
+	new Category('food', root_cat, 5),
+	new Category('electronic', root_cat, 18)
+]
+category_samples.push(new Category('Laptops', category_samples[category_samples.length-1]))
+exports.category_samples = category_samples
+exports.product_samples = [
+	new Product('Lays', category_samples[1], 'Frito', 10),
+	new Product('Asus ROG XDXD', category_samples[2], 'Asus', 100000)
+]
